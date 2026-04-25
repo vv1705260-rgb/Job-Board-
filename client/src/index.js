c6fe56acc3192af489cb22959e0a88dd5592319a
Vaishali <vv1705260@gmail.com>
2026-04-25 11:09:44 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,3 +4,8 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+
+
+            app.use(cors({
+  origin: ['http://localhost:3000', 'https://job-board-frontend-xxxx.vercel.app']
+}));
